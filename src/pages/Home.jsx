f451ff87fb7f1d85887488b1ac781e2db9947e67
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2025-04-10 11:24:46 -0400

    Added all elements to home mage and linkable clarkstables page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,30 @@
 export default function Home(second) {
+  function visitPage() {
+    window.location = "http://www.clarissafuller.com/clarkstables";
+  }
   return (
     <>
-      <button type="button">Portfolio</button>
-      <button type="button">Clark's Table</button>
+      <h2>welcome to</h2>
+      <h1>CLARISSAFULLER.COM</h1>
+      <p>you're probably here for one of two reasons:</p>
+      <div className="longtextbox">
+        <p>You're here to see my developer work</p>
+        <p>or</p>
+        <p>
+          You're curious about Clark's Tables, my world of food, florals, and
+          gatherings!
+        </p>
+      </div>
+      <p>
+        Whichever path you're here for, I'm so glad you stopped by. Choose
+        you're adventure below:
+      </p>
+      <div className="buttonbox">
+        <button type="button">Portfolio</button>
+        <button onClick={visitPage} type="button">
+          Clark's Table
+        </button>
+      </div>
     </>
   );
 }
